8e9d4829612bc746c771612a3d8692bae7dfa74e
Savini Rathnayake <109766827+vini2811@users.noreply.github.com>
2023-08-08 07:57:15 +0530

    Add files via upload

---
+++ Tutorial07/Tutorial07Q02.scala
@@ -0,0 +1,11 @@
+object Tutorial07Q02 {
+  def main(args: Array[String]): Unit = {
+    val inputList = List(1, 2, 3, 4, 5)
+    val outputList = calculateSquare(inputList)
+    println(outputList)
+  }
+
+  def calculateSquare (inputList: List[Int]): List[Int] = {
+    inputList.map(number => number*number)
+  }
+}
